6c4ecc1eb38ad70c454c0d92f6fe37aabf60657b
Zuxriddin Kamalov <bigjoss@mail.ru>
2019-10-10 13:41:09 +0500

    Refactoring

--- settings/settings_prod.py
+++ settings/settings_prod.py
@@ -21,7 +21,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT':  os.environ.get('DATABASE_PORT'),
     }
 }
